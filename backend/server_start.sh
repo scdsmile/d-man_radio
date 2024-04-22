70cd0bcf88e7f4a41bda0922411081963bec0bf1
scdsmile <parallelepipedstyle@gmail.com>
2024-04-22 20:08:08 +0300

    Добавлена игра

--- backend/server_start.sh
+++ backend/server_start.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-gunicorn --daemon -c gunicorn_config.py wsgi:app
+# проверка на наличие работающего процесса "app"
+if ps ax | grep -v grep | grep wsgi:app > /dev/null
+then
+    echo "app.py is already running! restart? (y/n)"
+    read user_choice
+
+    if [ "$user_choice" = "y" ]
+    then
+        echo "Restart app.py..."
+        pkill -f wsgi:app
+        gunicorn --daemon -c gunicorn_config.py wsgi:app
+        echo "App is succesfully restarted."
+    else
+        echo "No changes..."
+    fi
+else
+    echo "Starting app..."
+    gunicorn --daemon -c gunicorn_config.py wsgi:app
+    echo "App is succesfully started."
+fi
